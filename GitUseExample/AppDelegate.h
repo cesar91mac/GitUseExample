91c12f46d13b74c31361678a4a374cbdf6546067
Cesar Flores <cesar91.mac@gmail.com>
2013-12-29 22:40:59 -0600

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by César Flores on 12/29/13.
+//  Copyright (c) 2013 Cesar Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by César Flores on 12/29/13.
+//  Copyright (c) 2013 Cesar Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
